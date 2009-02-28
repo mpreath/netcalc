33b5b7f1a53088519cfb49ce84ca4b3c91e46205
Matt Reath <mpreath@gmail.com>
2009-02-28 08:05:20 -0600

    Added GPL header to network_tree.c

--- src/network_tree.c
+++ src/network_tree.c
@@ -1,3 +1,23 @@
+/* 
+    netcalc - a simple CLI subnet calculator written in ANSI C
+    network_tree.c - subnetting/supernetting related fuctions. It is better
+    to handle these functions as a tree structure
+
+    Copyright (C) 2009  Matthew Reath 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <network_tree.h>
 #include <utility.h>
 #include <network.h>
