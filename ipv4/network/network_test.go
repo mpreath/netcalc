1b201da810a8cad4819d2bc51659e6c47b15ff40
Matt Reath <mattreath@icloud.com>
2022-03-28 06:45:09 -0500

    updated network test

--- ipv4/network/network_test.go
+++ ipv4/network/network_test.go
@@ -1,26 +1,32 @@
 package network
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/mpreath/netcalc/ipv4"
+)
 
 func TestGenerateNetwork(t *testing.T) {
 	test_cases := []struct {
-		dd_address   string
-		uint_address uint32
-		dd_mask      string
-		uint_mask    uint32
+		dd_address         string
+		dd_network_address string
+		dd_mask            string
 	}{
-		{"192.168.1.0", 3232235776, "255.255.255.0", 4294967040},
+		{"192.168.1.1", "192.168.1.0", "255.255.255.0"},
 	}
 
 	for _, test_case := range test_cases {
 		test_network, _ := GenerateNetwork(test_case.dd_address, test_case.dd_mask)
 
-		if test_network.Address != test_case.uint_address {
-			t.Errorf("generated address (%d) doesn't match spec address (%d)", test_network.Address, test_case.uint_address)
+		dd_test_network, _ := ipv4.Itodd(test_network.Address)
+		dd_test_mask, _ := ipv4.Itodd(test_network.Mask)
+
+		if dd_test_network != test_case.dd_network_address {
+			t.Errorf("generated address (%s) doesn't match spec address (%s)", dd_test_network, test_case.dd_network_address)
 		}
 
-		if test_network.Mask != test_case.uint_mask {
-			t.Errorf("generated mask (%d) doesn't match spec mask (%d)", test_network.Mask, test_case.uint_mask)
+		if dd_test_mask != test_case.dd_mask {
+			t.Errorf("generated mask (%s) doesn't match spec mask (%s)", dd_test_mask, test_case.dd_mask)
 		}
 	}
 }
