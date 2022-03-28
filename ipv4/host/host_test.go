46b526f1471791e101f7afb5df8286ac957b0956
Matt Reath <mattreath@icloud.com>
2022-03-28 06:59:05 -0500

    added host test

---
+++ ipv4/host/host_test.go
@@ -0,0 +1,31 @@
+package host
+
+import (
+	"testing"
+
+	"github.com/mpreath/netcalc/ipv4"
+)
+
+func TestGenerateHost(t *testing.T) {
+	test_cases := []struct {
+		dd_address string
+		dd_mask    string
+	}{
+		{"192.168.1.1", "255.255.255.0"},
+	}
+
+	for _, test_case := range test_cases {
+		test_host, _ := GenerateHost(test_case.dd_address, test_case.dd_mask)
+
+		dd_test_address, _ := ipv4.Itodd(test_host.Address)
+		dd_test_mask, _ := ipv4.Itodd(test_host.Mask)
+
+		if dd_test_address != test_case.dd_address {
+			t.Errorf("generated address (%s) doesn't match spec address (%s)", dd_test_address, test_case.dd_address)
+		}
+
+		if dd_test_mask != test_case.dd_mask {
+			t.Errorf("generated mask (%s) doesn't match spec mask (%s)", dd_test_mask, test_case.dd_mask)
+		}
+	}
+}
