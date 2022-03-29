1545c23b47f32236ee46b7b32ab7bd7869e5c2ba
Matt Reath <mattreath@icloud.com>
2022-03-29 07:18:06 -0500

    organized root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -12,19 +12,29 @@ var rootCmd = &cobra.Command{
 	Use:   "netcalc",
 	Short: "Netcalc is a IPv4/IPv6 network calculator",
 	Run: func(cmd *cobra.Command, args []string) {
-		// do stuff here
-		n, err := network.GenerateNetwork("192.168.1.90", "255.255.255.252")
+		n, err := network.GenerateNetwork("192.168.1.1", "255.255.255.0")
 		if err != nil {
 			fmt.Print(err)
 			return
 		}
-		n_dd_address, _ := ipv4.Itodd(n.Address)
-		n_dd_mask, _ := ipv4.Itodd(n.Mask)
-		n_dd_bcast, _ := ipv4.Itodd(n.BroadcastAddress)
-		fmt.Printf("Network: { Address: \"%s\", Mask: \"%s\", BroadcastAddress: \"%s\", Host Count: %d }\n", n_dd_address, n_dd_mask, n_dd_bcast, len(n.Hosts))
+		printNetworkInformation(n)
 	},
 }
 
 func Execute() error {
 	return rootCmd.Execute()
+}
+
+func printNetworkInformation(n *network.Network) {
+	n_dd_address, _ := ipv4.Itodd(n.Address)
+	n_dd_mask, _ := ipv4.Itodd(n.Mask)
+	n_dd_bcast, _ := ipv4.Itodd(n.BroadcastAddress)
+
+	fmt.Printf("Network:\t%s\n", n_dd_address)
+	fmt.Printf("Mask:\t\t%s\n", n_dd_mask)
+	fmt.Printf("Broadcast:\t%s\n", n_dd_bcast)
+	fmt.Printf("Usable Hosts:\t%d\n", len(n.Hosts))
+}
+
+func printNetworkInformationJSON(network *network.Network) {
 }
