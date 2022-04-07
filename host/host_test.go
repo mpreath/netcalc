3b77f9092134e16c23b2eb5a8302f77526bc33ce
Matt Reath <mattreath@icloud.com>
2022-04-07 16:47:53 -0500

    improved host test coverage

--- host/host_test.go
+++ host/host_test.go
@@ -1,10 +1,33 @@
 package host
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/mpreath/netcalc/utils"
 )
+
+func TestMarshalJSON(t *testing.T) {
+	test_cases := []struct {
+		dd_address string
+		dd_mask    string
+	}{
+		{"192.168.1.1", "255.255.255.0"},
+	}
+
+	for _, test_case := range test_cases {
+		test_host, _ := GenerateHost(test_case.dd_address, test_case.dd_mask)
+
+		s, err := json.Marshal(test_host)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		if len(s) <= 0 {
+			t.Errorf("didn't receive any output from marshal")
+		}
+	}
+}
 
 func TestGenerateHost(t *testing.T) {
 	test_cases := []struct {
